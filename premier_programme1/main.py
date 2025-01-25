50a23ed4891575e553a8b207010ee1572f9e3d88
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-01-25 23:59:23 +0100

    premier test

---
+++ premier_programme1/main.py
@@ -0,0 +1,15 @@
+# Variable bom
+bom = "Bom dia"
+print("Je m'appelle toto. " + bom) 
+print("Mon nom c'est bien toto.")
+
+profession = "Développeur"
+prenom = "Jean"
+nom = "Dupont"
+print("Prénom :")
+print(prenom)
+print("Nom :")
+print(nom)
+print ()
+print ("Identité : " + prenom + " " + nom + " (" + profession + ")")
+print (f"Identité : {prenom} {nom} ({profession})")
