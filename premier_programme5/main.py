6c0af26ecf821c14d82f0cf8c064bb82930601b4
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-03-21 21:39:54 +0100

    Update main.py

--- premier_programme5/main.py
+++ premier_programme5/main.py
@@ -5,4 +5,4 @@ apprendre la programmation"""
 nom = input("Quel est votre nom ? ")
 age = input("Quel est votre age ? ")
 print("Vous vous appelez " + nom + ", vous avez " + age + " ans")
-# print("Merci, passez une bonne journée")
+print(f"Merci, passez une bonne journée {nom} !")
